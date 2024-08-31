6085c2c35bd1fcca36e91bcf9fcbbd12f5260326
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-08-31 23:53:09 +0530

    While Loop

--- Control Flow/index.js
+++ Control Flow/index.js
@@ -1,4 +1,16 @@
-let students = ["subham", "alex", "mark" , "deepak"];
+// *** Stat for loop
+// let students = ["subham", "alex", "mark" , "deepak"];
+
+// const student = {
+//     username : "Nimesh",
+//     age : 24,
+// }
+
+// update the array
+// students.push("harvey");
+
+// upadte the arrray in posotion
+// students[6] = "Nimesh";
 
 // for loop to print arrays
 
@@ -15,11 +27,71 @@ let students = ["subham", "alex", "mark" , "deepak"];
  
 // access the arrry elments with array.length for arrays
 
-for(let i = 0; i < students.length ; i++){
-    console.log(students[i]);
+// for(let i = 0; i < students.length ; i++){
+//     console.log(students[i]);
     
+// }
+    
+// of in arrays Accses to key(Acces empty values)
+
+// for(let student of students){
+//        console.log(student)
+// }
+
+// in keyword - Acces to key (Skips empty values)
+// for(let index in students){
+//     console.log(index);
+// }
+
+// for(let key in student){
+//     console.log(student[key]);
+// }
+
+// print character of string
+// const username = "Nimesh";
+
+// for(let character of username){
+//     console.log(character);
+// }
+
+
+// console.log("out of loop");
+
+// ******** End of the For loop
+
+
+
+// *** Start of While Loop
+
+
+let students = ["subham", "alex", "mark" , "deepak"];
+
+// Syntax of While loop
+
+// let count = 0; // initialyzation
+// while(count < 5){  // condition
+//     console.log("Hello!");
+//     count ++; // increment
+// }
+
+// infinaitve loop
+
+// let x = 0; // initialyzation
+// while(true){  // condition
+//     console.log("Hello!");
+
+// }
+
+//  print array elements
+
+
+let i = 0;
+while(i < students.length){
+    console.log(students[i]);
+    i++;
 }
 
+// *** end of while loop
 
+// ** Start of do while loop
 
-console.log("out of loop");
