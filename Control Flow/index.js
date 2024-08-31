ea74f3c6f5bb89c9c30a9629ff3aabfe5c1b2c16
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-08-31 23:20:52 +0530

    For Loop

---
+++ Control Flow/index.js
@@ -0,0 +1,25 @@
+let students = ["subham", "alex", "mark" , "deepak"];
+
+// for loop to print arrays
+
+// for(let count = 0; count < 5; count++){
+//   console.log("Hello!");
+// }  
+
+// access array elemnts when the lenght is known
+
+//    for(let i = 0; i < 4; i++ ){
+//          console.log(students[i]);
+//    }
+   
+ 
+// access the arrry elments with array.length for arrays
+
+for(let i = 0; i < students.length ; i++){
+    console.log(students[i]);
+    
+}
+
+
+
+console.log("out of loop");
