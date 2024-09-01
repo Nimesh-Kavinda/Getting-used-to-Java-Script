edaf5b0b31d99856a08ec8ac10dcadc617c965e7
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-01 11:09:57 +0530

    Argument Parameters

---
+++ Functions/index.js
@@ -0,0 +1,70 @@
+// function is s block of code
+
+// ** function part 01
+
+// ** exmaple
+
+// function addition(numOne, numTow){
+//     return numOne + numTow;
+// }
+// let result = addition(5,8);
+// console.log(result);
+
+// console.log(addition(10,67));
+
+// ** function part 02
+
+//  function intro(){
+//     console.log("Welcome to the Corse!"); // declare function
+//  }
+
+//  intro(); // calling function
+
+// storing function in a variabel
+// let intro = function(){
+//       console.log("Welacome to the corse!");
+// };
+
+// intro();
+
+
+// ** hoyisting - A function can use befor the declaration
+  
+// let random = 20;
+//   console.log(random); // geanaral way
+
+// intro();
+
+// function intro(){
+//     console.log("Welcom to the corse!")
+// }
+
+
+// function intro(){
+//     console.log("Welcom to the corse!")
+// }
+// intro();
+
+
+// ** Start of Arguement Parameters 
+
+// function welcome(name){  // parameter == name
+//     console.log(`Welcome ${name}`)
+// }
+
+// welcome("shubham");  // argumnent to parameter = "Shubham"
+
+//  function addition(numOne, numTow){
+//     let result = numOne + numTow;
+//     console.log(result);
+//  }
+//  addition(5,10);
+                                      //defulte value
+function fullName(firstName ="ABC", lastName ="XYZ"){
+    console.log(`Welcome ${firstName} ${lastName}`);
+}
+
+fullName("Nimesh", "Kavinda");
+fullName();
+
+//** End of Arguement Parameters */
