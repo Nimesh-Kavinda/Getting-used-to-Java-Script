541850b2903aa793bf9851ace84be67bf80573ee
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-02 13:43:19 +0530

    Query Selectors and Selectors

---
+++ 06-Document Object Model/inedx.js
@@ -0,0 +1,15 @@
+//** query selector */
+
+// let description = document.querySelectorAll('p');
+// description.forEach(paragraph => console.log(paragraph));
+
+//** selectors */
+
+// let selector = document.getElementById("website-name");
+// console.log(selector);
+
+// let selectorClass = document.getElementsByClassName("active");
+// // console.log(selectorClass);
+
+// let selectorTag = document.getElementsByTagName("p");
+// console.log(selectorTag);
