cc1ce18e8f48f20047c3da71c230902f4e95b335
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-04 23:05:11 +0530

    Filter method

---
+++ 09-Array Methods/index.js
@@ -0,0 +1,35 @@
+ //**Filter Methods  */
+
+//  const ratings = [5, 10, 9, 7, 2, 3, 8];
+
+//  const filterdRatings = ratings.filter(rate => rate > 6);
+
+//  console.log(filterdRatings);
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// const even = numbers.filter((num) => {
+//     let result = num % 2 == 0;
+//     return result;
+// });
+
+// console.log(even);
+
+// const odd = numbers.filter((num) => {
+//     let result = num % 2 == 0;
+//     return !result;
+// });
+// console.log(odd);
+
+
+const movies = [
+    {name:"Coffe Day", rating:9},
+    {name:"The Lost Coede", rating:7},
+    {name:"Typical Bug", rating:4}
+];
+
+const filterMovies = movies.filter((movie) => {
+    return movie.rating > 6;
+
+});
+
+console.log(filterMovies);
