62abb140f11c7a5489535ff3a2d8ee796010bdc6
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-06 01:17:50 +0530

    Adding Task

---
+++ 10-Project ToDo List/index.js
@@ -0,0 +1,20 @@
+//** Add task */
+
+const addForm =document.querySelector(".add");
+const tasks = document.querySelector(".tasks");
+
+addForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const value = addForm.task.value.trim();
+    
+    if(value.length){
+
+        tasks.innerHTML += ` <li>
+                            <span>${value}</span>
+                            <i class="bi bi-trash-fill delete"></i>
+                            </li>`;
+
+        addForm.reset();
+    }
+
+});
