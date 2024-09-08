1988b8aca0bfe923a409aaa80c6df283e93837f0
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-08 17:25:37 +0530

    Create Add transaction function

---
+++ 13-Project-Expence-Traker/index.js
@@ -0,0 +1,13 @@
+//** Add transaction */
+
+const form = document.querySelector(".add");
+
+form.addEventListener("submit", event =>{
+    event.preventDefault();
+    const transaction = {
+        source: form.source.value,
+        amount: form.amount.value,
+    };
+    localStorage.setItem("transaction", JSON.stringify(transaction));
+});
+
