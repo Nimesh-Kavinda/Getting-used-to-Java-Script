5cc3bb259db24da90697d36abe9c37ef14ac8f27
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-10 00:19:37 +0530

    Objject Literals in JS

--- 14-OOP-JS/index.js
+++ 14-OOP-JS/index.js
@@ -1,2 +1,25 @@
 //** video 1 == Instruction */
 //** Video 2 == Instruction Using Console in Browser */
+
+//** Object Literals */
+
+const userOne = {
+    userName: "Nimesh",
+    fullName: "Kavinda",
+    email : "nimesh@gamil.com",
+    age : 24,
+
+    login(){
+        console.log("LOGIN")
+    },
+    logout(){
+        console.log("LogOut")
+    }
+};
+
+console.log(userOne.age);
+console.log(userOne.userOne);
+
+const userTow{
+    userName : "";
+}
