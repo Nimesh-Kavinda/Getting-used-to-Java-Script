d34c6f1a8309fb52c307ab8eb1e6a225bbab42cd
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-11 00:55:32 +0530

    Classes (Syntax)

--- 14-OOP-JS/index.js
+++ 14-OOP-JS/index.js
@@ -3,23 +3,65 @@
 
 //** Object Literals */
 
-const userOne = {
-    userName: "Nimesh",
-    fullName: "Kavinda",
-    email : "nimesh@gamil.com",
-    age : 24,
+// const userOne = {
+//     userName: "Nimesh",
+//     fullName: "Kavinda",
+//     email : "nimesh@gamil.com",
+//     age : 24,
 
-    login(){
-        console.log("LOGIN")
-    },
-    logout(){
-        console.log("LogOut")
+//     login(){
+//         console.log("LOGIN")
+//     },
+//     logout(){
+//         console.log("LogOut")
+//     }
+// };
+
+// console.log(userOne.age);
+// console.log(userOne.userName);
+// userOne.login();
+
+
+
+// const userTwo = {
+//     userName: "Kavinda",
+//     fullName: "Dimuth",
+//     email : "kavih@gamil.com",
+//     age : 28,
+
+//     login(){
+//         console.log("LOGIN")
+//     },
+//     logout(){
+//         console.log("LogOut")
+//     }
+// };
+
+// console.log(userTwo.age);
+// console.log(userTwo.userName);
+// userTwo.login();
+
+
+// const userFive =new User("adithya", "Kavi", "adikavi@gmail.com", 35);
+// console.log(userFive.userName);
+
+//** Classes */
+
+class User {
+    constructor(username, age){
+        this.username = username;
+        this.age = age;
     }
-};
-
-console.log(userOne.age);
-console.log(userOne.userOne);
-
-const userTow{
-    userName : "";
 }
+
+const userOne = new User("Kavinda", 32);
+console.log(userOne);
+
+const userTwo = new User("Adithya", 35);
+console.log(userTwo);
+
+
+console.log(userOne.username);
+
+const userThree = new User("Dimuth", 24);
+console.log(userThree.username);
