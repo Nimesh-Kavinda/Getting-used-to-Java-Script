fe757f9c4dc327f30d35fffd604b2f9bd68efb6c
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-09-11 12:37:38 +0530

    Prototype Models and Inheritance

--- 14-OOP-JS/index.js
+++ 14-OOP-JS/index.js
@@ -113,58 +113,117 @@
 
 //** Inheritance */
 
-class User {
-    constructor(username, age){
-        this.username = username;
-        this.age = age;
-        this.active = false;
-    }
-    login(){
-        this.active = true;
-        return this;
+// class User {
+//     constructor(username, age){
+//         this.username = username;
+//         this.age = age;
+//         this.active = false;
+//     }
+//     login(){
+//         this.active = true;
+//         return this;
         
-    }
-    logout(){
-        this.active = false;
-        return this;
-    }
-    display(){
-        if(this.active){
-        console.log(`Username: ${this.username}, Age: ${this.age}`);
+//     }
+//     logout(){
+//         this.active = false;
+//         return this;
+//     }
+//     display(){
+//         if(this.active){
+//         console.log(`Username: ${this.username}, Age: ${this.age}`);
 
-        }else{
-            console.log("Please login first");
-        }
-    }
+//         }else{
+//             console.log("Please login first");
+//         }
+//     }
+// }
+
+// class Student extends User{
+//     constructor(username, age, semester, rank){
+//         super(username, age);
+//         this.semester = semester;
+//         this.rank = rank;
+//     }
+//     updateRank(newRank){
+//         this.rank = newRank;
+//         console.log(this.rank);
+//     } 
+//     display(){
+//         if(this.active){
+//         console.log(`Username: ${this.username}, Age: ${this.age} Semester: ${this.semester}, Rank: ${this.rank} `);
+
+//         }else{
+//             console.log("Please login first");
+//         }
+//     }
+// }
+
+
+
+// const userOne = new User("Kavinda", 32);
+// userOne.login().display();
+
+// const studentOne = new Student("Jone", 60, 5, 10);
+// studentOne.login().display();
+
+
+//** Prototype Model */
+
+// function User(username, age){
+//     this.username = username;
+//     this.age = age;
+//     this.active = false;
+// }
+
+// User.prototype.login = function(){
+//     console.log("Login");
+// }
+
+// User.prototype.logout = function(){
+//     console.log("Logout");
+// }
+
+// const userOne = new User("Nimesh", 35);
+// console.log(userOne.username);
+// userOne.logout();
+
+//** Prototype Inheritance */
+
+function User(username, age){
+    this.username = username;
+    this.age = age;
+    this.active = false;
 }
 
-class Student extends User{
-    constructor(username, age, semester, rank){
-        super(username, age);
-        this.semester = semester;
-        this.rank = rank;
-    }
-    updateRank(newRank){
-        this.rank = newRank;
-        console.log(this.rank);
-    } 
-    display(){
-        if(this.active){
-        console.log(`Username: ${this.username}, Age: ${this.age} Semester: ${this.semester}, Rank: ${this.rank} `);
+User.prototype.login = function(){
+    console.log("Login");
+}
 
-        }else{
-            console.log("Please login first");
-        }
-    }
+User.prototype.logout = function(){
+    console.log("Logout");
+}
+
+function Student(username, age, semester, rank){
+    User.call(this, username, age);
+    this.semester =semester;
+    this.rank = rank;
+}
+
+Student.prototype = Object.create(User.prototype);
+
+Student.prototype.updateRank = function(newRank){
+    this.rank = newRank;
+    console.log(this.rank);
 }
 
 
+console.log(User.prototype);
+console.log(Student.prototype);
 
-const userOne = new User("Kavinda", 32);
-userOne.login().display();
+const userOne = new User("Nimesh", 35);
+console.log(userOne.username);
+userOne.logout();
 
-const studentOne = new Student("Jone", 60, 5, 10);
-studentOne.login().display();
-
-
-
+const studentOne = new Student("jone", 60, 5 ,12);
+console.log(studentOne.username);
+studentOne.updateRank(16);
