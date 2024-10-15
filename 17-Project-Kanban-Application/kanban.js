eacf35c44aa9a98b23427c117dc38b045fbb4c0d
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-10-15 15:44:01 +0530

    Class functions

---
+++ 17-Project-Kanban-Application/kanban.js
@@ -0,0 +1,43 @@
+export default class Kanban {
+
+    static getTasks(columnID){
+
+    }
+
+    static insertTask(columnId, content){
+
+    }
+
+    static updateTask(taskId, updatedInformation){
+
+    }
+
+    static deleteTask(taskId){
+
+    }
+
+    static getAllTask(){
+        const data = read();
+        return [data[0].tasks, data[1].tasks, data[2].tasks];
+    }
+
+    
+}
+
+function read(){
+    const data = localStorage.getItem("data");
+
+    if(!data){
+        return [
+             { columnId: 0, tasks: [] },
+             { columnId: 1, tasks: [] },
+             { columnId: 2, tasks: [] } 
+        ];
+    }
+
+    return JSON.parse(data);
+}
+
+function save(){
+
+}
