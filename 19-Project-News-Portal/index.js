7c3406a052621f84c4cbeef969958ff170de396f
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-10-20 11:31:26 +0530

    Worked with News Categories

--- 19-Project-News-Portal/index.js
+++ 19-Project-News-Portal/index.js
@@ -1,9 +1,19 @@
 const cards = document.querySelector(".cards");
-const url = "https://newsapi.org/v2/everything?q=tesla&from=2024-09-20&sortBy=publishedAt&apiKey=68b1a09ae71249feaf154fee6547c822";
+const category = document.querySelector(".category");
+const categorySpan = document.querySelectorAll(".category span");
+
+const baseUrl = "https://newsapi.org/v2";
+const apiKey = "&apiKey=68b1a09ae71249feaf154fee6547c822";
+
+// const urlUa = "https://newsapi.org/v2/top-headlines?country=us&apiKey=68b1a09ae71249feaf154fee6547c822";
+// const urlUb = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=68b1a09ae71249feaf154fee6547c822";
+// const urlT = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=68b1a09ae71249feaf154fee6547c822";
+// const urlC = "https://newsapi.org/v2/everything?q=crypto&sortBy=publishdAt&apiKey=68b1a09ae71249feaf154fee6547c822";
+
 
 async function dataRequest(url){
     try{
-        const response = await fetch(url);
+        const response = await fetch(baseUrl + url + apiKey);
         const json = response.json();
         return json;
     } catch(error){
@@ -11,27 +21,43 @@ async function dataRequest(url){
     }
 }
 
-dataRequest(url).then(data => {
-data.articles.forEach(item => {
-    cards.innerHTML += `<div class="card">
-                            <div class="image">
-                            <img src="${item.urlToImage}" alt="Default News Image">
-                            </div>
-                            <div class="information">
-                            <div>
-                            <p class="title">${item.title}</p>
-                            <p class="description">${item.description}</p>
-                            <p class="time">
-                                <span>${item.publishedAt.replace("Z", "").split("T")[1]}</span>
-                                <span>${item.publishedAt.replace("Z", "").split("T")[0]}</span>
-                            </p>
-                            </div>
-                            <div class="other">
-                                <span class="source">${item.source.name}</span>
-                                <a class="url" href="${item.url}" target="_blank"> Read Article <i class="bi bi-arrow-right"></i> </a>
-                            </div>
-                        </div>
-                       </div>
-                    </div>`;
-});
-});
+function urlRequest(url){
+
+    dataRequest(url).then(data => {
+        data.articles.forEach(item => {
+            cards.innerHTML += `<div class="card">
+                                    <div class="image">
+                                    <img src="${item.urlToImage}" alt="Default News Image">
+                                    </div>
+                                    <div class="information">
+                                    <div>
+                                    <p class="title">${item.title}</p>
+                                    <p class="description">${item.description}</p>
+                                    <p class="time">
+                                        <span>${item.publishedAt.replace("Z", "").split("T")[1]}</span>
+                                        <span>${item.publishedAt.replace("Z", "").split("T")[0]}</span>
+                                    </p>
+                                    </div>
+                                    <div class="other">
+                                        <span class="source">${item.source.name}</span>
+                                        <a class="url" href="${item.url}" target="_blank"> Read Article <i class="bi bi-arrow-right"></i> </a>
+                                    </div>
+                                </div>
+                               </div>
+                            </div>`;
+        });
+        });
+
+}
+
+category.addEventListener("click" , event => {
+    if(event.target.tagName === "SPAN"){
+       cards.innerHTML = "";
+       urlRequest(event.target.dataset.id);
+       categorySpan.forEach(item => item.classList.remove("active"));
+       event.target.classList.add("active");
+    }
+})
+
+urlRequest("/top-headlines?country=us");
+
