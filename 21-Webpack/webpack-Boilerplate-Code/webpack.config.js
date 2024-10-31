518ffa1795d6f31791f72b7e271d6ae307bfe063
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-01 00:13:21 +0530

    Creat a webpack-Boilerplate-Code Template for Projects

---
+++ 21-Webpack/webpack-Boilerplate-Code/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist/assets"),
+        filename : "./assets/bundel.js",
+    },
+    module: {
+        rules: [
+            {
+                  test:/\.js$/,
+                  exclude:/node_modules/,
+                  use:"babel-loader"
+            }
+        ]
+    },
+    devServer: {
+        static:{
+            directory: path.resolve(__dirname, "dist"),
+            watch: true
+        },
+        port: 8080,
+        hot: true,
+        open : true
+    }
+}
